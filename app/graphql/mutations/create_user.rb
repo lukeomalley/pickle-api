e925f976a349c535e38b5ae238b02c691a0e9c58
Luke O'Malley <Lukeomalley95@gmail.com>
2019-09-18 16:18:52 -0400

    implemented create user mutation

---
+++ app/graphql/mutations/create_user.rb
@@ -0,0 +1,21 @@
+class Mutations::CreateUser < Mutations::BaseMutation
+  argument :name, String, required: true
+  argument :username, String, required: true
+  argument :email, String, required: false
+  argument :bio, String, required: false
+  argument :img_url, String, required: false
+
+  field :user, Types::UserType, null: false
+  field :errors, [String], null: false
+
+  def resolve(name:, username:, email:, bio:, img_url:)
+    user = User.new(name: name, username: username, email: email, bio: bio, img_url: img_url)
+    if user.save
+      # user successfully created! return the user and no errors
+      { user: user, errors: [] }
+    else
+      # user failed to save return nil user and errors
+      { user: nil, errors: user.errors.full_messages }
+    end
+  end
+end
