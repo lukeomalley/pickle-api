41a0bd73f2893967596bbe1126eacecb835981e5
Luke O'Malley <Lukeomalley95@gmail.com>
2019-09-18 12:57:39 -0400

    setup graphql and graphiql namespacing workking with testquery

--- app/controllers/api/v1/graphql_controller.rb
+++ app/controllers/api/v1/graphql_controller.rb
@@ -1,4 +1,4 @@
-class GraphqlController < ApplicationController
+class Api::V1::GraphqlController < ApplicationController
   def execute
     variables = ensure_hash(params[:variables])
     query = params[:query]
