b173800afc1b6466a7124161303421aac0763512
Luke O'Malley <Lukeomalley95@gmail.com>
2019-09-18 16:44:59 -0400

    implemented create_selection mutation

---
+++ app/graphql/mutations/create_selection.rb
@@ -0,0 +1,27 @@
+module Mutations
+  class CreateSelection < Mutations::BaseMutation
+    argument :user_id, ID, required: true
+    argument :option_id, ID, required: true
+
+    field :success, Boolean, null: true
+    field :errors, [String], null: false
+
+    def resolve(user_id:, option_id:)
+      user = User.find(user_id)
+      option = Option.find(option_id)
+      selection = Selection.new(user: user, option: option)
+
+      if selection.save
+        {
+          success: true,
+          errors: []
+        }
+      else
+        {
+          success: false,
+          errors: selection.errors.full_messages
+        }
+      end
+    end
+  end
+end
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -3,5 +3,7 @@
 module Types
   class MutationType < Types::BaseObject
     field :create_user, mutation: Mutations::CreateUser
+    field :create_comment, mutation: Mutations::CreateComment
+    field :create_selection, mutation: Mutations::CreateSelection
   end
 end
