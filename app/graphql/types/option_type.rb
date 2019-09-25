440a858efde476a36e20c575958f11bff650608e
Luke O'Malley <Lukeomalley95@gmail.com>
2019-09-24 22:07:04 -0400

    updated graphql type for selections

--- app/graphql/types/option_type.rb
+++ app/graphql/types/option_type.rb
@@ -5,6 +5,8 @@ module Types
     field :id, ID, null: false
     field :text, String, null: true
     field :img_url, String, null: true
+    field :percentage, Float, null: true
+
     field :selections, [Types::SelectionType], null: true
   end
 end
--- app/models/option.rb
+++ app/models/option.rb
@@ -5,7 +5,7 @@ class Option < ApplicationRecord
   has_many :selections, dependent: :destroy
 
   def percentage
-    option_selections = Selection.where(option_id: Option.last.id).count.to_f
+    option_selections = Selection.where(option_id: id).count.to_f
     total_selections = pickle.selections.count.to_f
     return 0 if total_selections.zero?
 
