eb1b7caf3232bb432b5e48436e1e5532a4364c44
Luke O'Malley <Lukeomalley95@gmail.com>
2019-09-24 21:56:38 -0400

    implemented percentage to option model

--- app/models/option.rb
+++ app/models/option.rb
@@ -3,4 +3,10 @@
 class Option < ApplicationRecord
   belongs_to :pickle
   has_many :selections, dependent: :destroy
+
+  def percentage
+    option_selections = Selection.where(option_id: Option.last.id).count.to_f
+    total_selections = pickle.selections.count.to_f
+    (option_selections / total_selections) * 100
+  end
 end
