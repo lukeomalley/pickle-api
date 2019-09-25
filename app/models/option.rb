552c82387ef3acf5d3f75e16555de8d2c7846bb5
Luke O'Malley <Lukeomalley95@gmail.com>
2019-09-24 21:57:42 -0400

    updated percentage to check for the case of no selections

--- app/models/option.rb
+++ app/models/option.rb
@@ -7,6 +7,8 @@ class Option < ApplicationRecord
   def percentage
     option_selections = Selection.where(option_id: Option.last.id).count.to_f
     total_selections = pickle.selections.count.to_f
+    return 0 if total_selections.zero?
+
     (option_selections / total_selections) * 100
   end
 end
